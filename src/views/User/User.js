35151338a1ee961847c8f2cb80cfca7672d8eb86
hui han chin <hchin@huis-MacBook-Pro.local>
2018-06-30 14:15:26 +0800

    mount bug

--- src/views/User/User.js
+++ src/views/User/User.js
@@ -15,42 +15,11 @@ import Widget02 from '../../views/Widgets/Widget02'
 
 import profile from '../../assets/img/profile/lynnette.jpeg'
 import { NetworkGraph } from '../../assets/data/NetworkGraph.new'
-
+import axios from 'axios';
 //TODO: Get this generated
-const topics = [
-  {
-    'topic': 'ai',
-    'likes': "3",
-    'seen': "15",
-    'postContent': "UK announces £1b drive into artificial intelligence",
-    'postLink': "https%3A%2F%2Fwww.businesstimes.com.sg%2Fgovernment-economy%2Fuk-announces-%C2%A31b-drive-into-artificial-intelligence&h=ATP4bL2ixfvEBGwBq72MPeLLoQiOKg_3eHfNCPt1D4-GurgJqHIpAkfIuu_2XiZK2igSIp1tEBKYarO9eZA5rlnDqnRRLGzUBl-GbughSCZ5qQUx2iEOpa9b0jBMn7Xa84xDMVk",
-    'postDate': "2018-04-27 09:15:00"
-  },
-  {
-    'topic': 'voice',
-    'likes': "1",
-    'seen': "17",
-    'postContent': "In pursuit of the perfect AI voice",
-    'postLink': "http%3A%2F%2Fflip.it%2FdbGLMs&h=ATPVk9vcR1tVMdYd6DEZU-BhWn0AGJ9sDuTfJArTyzMieNpPS1GhQ-SvThsdsJAPnAUdMCZpbnq9mnNHDZSsnY88SLDtLdmHK74wjrbWgk3fyJsf9tSknk3-EDE-bWlVd5I1OIg",
-    'postDate': "2018-04-19 08:05:00"
-  },
-  {
-    'topic': 'nus enterprise',
-    'likes': "2",
-    'seen': "19",
-    'postContent': "More happenings in Blk 71	NUS Enterprise and Singtel Innov8 launch new hub for cybersecurity startups",
-    'postLink': "https%3A%2F%2Fwww.opengovasia.com%2Farticles%2Fnus-enterprise-and-singtel-innov8-launch-new-hub-for-cybersecurity-startups&h=ATP3EhB23lyAgydx4njuI42qkYs0uKc-tkU5AmDg3lj_zB1d_46ASIF520LqZ-NwjFhuxyFwyeA65aM49f2Es0ZSMA2ROIi4S_L-suJXNkLxhQoe-n4ntLNs8oYxGJ7S0ooCkgs",
-    'postDate': "2018-04-03 23:46:00"
-  },
-  {
-    'topic': 'ai imaging technique',
-    'likes': "2",
-    'seen': "17", 
-    'postContent': "Nvidia's AI Imaging Technique Can Repair Damaged Photos Realistically",
-    'postLink': "https%3A%2F%2Fbeebom.com%2Fnvidia-feature-repair-images%2F&h=ATND9K2Pusn5l0grvny39IyNOsDz9xbxSHII4_jW84Rou7DXxFYKvtsaMj_r2NMiIHDIzojZI8YDNXZrikWVprXG8ffkHf7uYoIOddUeQayFqsUG7BqUL8jp_C77F5cR3U_7Lt4",
-    'postDate': "2018-04-25 09:15:00"
-  }  
-]
+
+
+
 
 const radar = {
   labels: ['Web Development', 'Data Science', 'Data Engineering', 'Web Design', 'Coding'],
@@ -162,8 +131,25 @@ class User extends Component {
     };
   }
 
-  render() {
+  setStateAsync(state) {
+    return new Promise((resolve) => {
+      this.setState(state, resolve)
+    });
+  }
 
+  async componentWillMount() {
+  const url = "data/user_dummy.json";
+  const res = await axios.get(url);
+  const jsonData = await res.data;
+  console.log(jsonData)
+  await this.setStateAsync({data: jsonData});
+  await this.setStateAsync({dataLoaded: true});
+  }
+
+
+  render() {
+    console.log(this.state.data)
+    const topics = this.state.data["topics"]
     return (
       <div className="animated fadeIn">
       
